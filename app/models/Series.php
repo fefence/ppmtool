e3d22fd856ba66c0e9ea2b6895fa5a8696c49dcf
veseto <wpopowa@gmail.com>
2014-12-05 15:39:07 +0200

    series calc

--- app/models/Series.php
+++ app/models/Series.php
@@ -15,7 +15,8 @@ class Series extends Eloquent
             ->orderBy('date_time', 'asc')
             ->get(array('id', 'short_result', 'home', 'away', 'date_time', 'home_goals', 'away_goals'));
         foreach ($matches as $match) {
-            echo $match->id . "<br>";
+            if ($match->short_result == '-' || $match->short_result == '')
+                break 1;
             for ($i = 1; $i < 11; $i++) {
                 $series = Series::where('league_id', '=', $league_id)->where('active', '=', 1)->where('game_type_id', '=', $i)->first();
                 if ($series == NULL) {
@@ -28,23 +29,24 @@ class Series extends Eloquent
                     $series->save();
                 }
                 $series->end_match_id = $match->id;
-                if (Match::endSeries([$match], $i)) {
-                    $series->active = 0;
-                    $duplicate = Series::where('start_match_id', '=', $series->start_match_id)
-                        ->where('end_match_id', '=', $series->end_match_id)
-                        ->where('league_id', '=', $league_id)
-                        ->where('length', '=', $series->length)
-                        ->where('game_type_id', '=', $series->game_type_id)->first();
-                    if ($duplicate) {
-                        $duplicate->delete();
+                if (Match::updated([$match])) {
+                    if (Match::endSeries([$match], $i)) {
+                        $series->active = 0;
+                        $duplicate = Series::where('start_match_id', '=', $series->start_match_id)
+                            ->where('end_match_id', '=', $series->end_match_id)
+                            ->where('league_id', '=', $league_id)
+                            ->where('length', '=', $series->length)
+                            ->where('game_type_id', '=', $series->game_type_id)->first();
+                        if ($duplicate) {
+                            $duplicate->delete();
+                        }
+                    } else {
+                        $series->length = $series->length + 1;
                     }
-                } else {
-                    $series->length = $series->length + 1;
                 }
                 $series->save();
             }
-            if ($match->short_result == '-')
-                break 1;
+
         }
 //        }
     }
--- app/routes.php
+++ app/routes.php
@@ -11,12 +11,21 @@
 |
 */
 
-Route::get('boo/', function()
+Route::get('boo/{i}/{season}', function($i, $season)
 {
+//    $ids = [];
+//    for($i = 1; $i < 10; $i ++) {
+        Series::calculateSeries($i, $season);
+        $seris = Series::where('active', 1)->get();
+        foreach($seris as $s){
+            $s->active = 0;
+            $s->save();
+        }
+//    }
 //    Updater::update(2);
-    $matches = Match::where('id', '2Vs6PNaA')->get();
-    $next =  Updater::getNextMatches($matches);
-    return $next->last();
+//    $matches = Match::where('id', '2Vs6PNaA')->get();
+//    $next =  Updater::getNextMatches($matches);
+//    return $next->last();
 //    return Parser::updateMatchesResult($matches);
 });
 
