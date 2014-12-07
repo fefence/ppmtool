e73bce88a281f970092e771062e23777e82ba354
veseto <wpopowa@gmail.com>
2014-12-07 17:52:27 +0200

    links in series and settigns

--- app/views/ppm.blade.php
+++ app/views/ppm.blade.php
@@ -4,7 +4,7 @@
 
 <table class="table">
     <thead>
-        <th style="width: 75px;"><a href="/settings" role="button" class="btn btn-default btn-xs">settings</a></th>
+        <th style="width: 12%;"><a href="/settings" role="button" class="btn btn-default btn-xs">settings</a></th>
         @foreach($games as $game)
             <th class="text-center">{{$game->name}}</th>
         @endforeach
@@ -12,7 +12,7 @@
     <tbody>
     @foreach($data as $c => $d)
     <tr>
-        <td><img src="/images/32/{{$c}}.png">&nbsp;{{$c}}</td>
+        <td><img src="/images/32/{{$c}}.png">&nbsp;{{$c}}&nbsp;<a href="http://www.sportstats.com/soccer/{{$d[1]['country']}}/{{$d[1]['name']}}">[ss]</a>&nbsp;<a href="http://www.betexplorer.com/soccer/{{$d[1]['country']}}/{{$d[1]['name']}}">[be]</a></td>
             @for($i = 1; $i < 11; $i ++)
                 <td class="text-center">{{$d[$i]['length']}}</td>
             @endfor
--- app/views/settings.blade.php
+++ app/views/settings.blade.php
@@ -4,7 +4,7 @@
 
 <table class="table">
     <thead>
-    <th style="width: 75px;"></th>
+    <th style="width: 12%;"><a href="/series" role="button" class="btn btn-default btn-xs">series</a></th>
     @foreach($game_types as $game_type)
         <th class="text-center">{{$game_type->name}}</th>
     @endforeach
@@ -12,7 +12,7 @@
     <tbody>
     @foreach($leagues as $league)
         <tr>
-            <td><img src="/images/32/{{$league->country_alias}}.png">&nbsp;{{$league->country_alias}}</td>
+            <td><img src="/images/32/{{$league->country_alias}}.png">&nbsp;{{$league->country_alias}}&nbsp;<a href="http://www.sportstats.com/soccer/{{$league->country}}/{{$league->name}}">[ss]</a>&nbsp;<a href="http://www.betexplorer.com/soccer/{{$league->country}}/{{$league->name}}">[be]</a></td>
         @foreach($game_types as $game_type)
             <td class="text-center">
                 @if(array_key_exists($league->id, $data) && in_array($game_type->id, $data[$league->id]))
