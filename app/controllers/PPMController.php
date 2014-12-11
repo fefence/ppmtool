fa9dfdc1213344536ab45a010a3f456e8bdcdddc
veseto <wpopowa@gmail.com>
2014-12-11 18:56:47 +0200

    tooltip for all series

--- app/controllers/PPMController.php
+++ app/controllers/PPMController.php
@@ -9,6 +9,16 @@ class PPMController extends \BaseController{
         $data = array();
         foreach($countries as $country) {
             for($i = 1; $i < 11; $i ++) {
+                $top = '';
+                $top_25 = Series::where('country_alias', $country)
+                    ->join('leagues', 'leagues.id', '=', 'series.league_id')
+                    ->where('game_type_id', $i)
+                    ->orderBy('length', "desc")
+                    ->take(25)
+                    ->lists('length');
+                foreach($top_25 as $t) {
+                    $top = $top.$t.", ";
+                }
                 $data[$country][$i] = Series::where('active', 1)
                     ->where('country_alias', $country)
                     ->join('leagues', 'leagues.id', '=', 'series.league_id')
@@ -17,6 +27,7 @@ class PPMController extends \BaseController{
                 if (count($data[$country][$i]) == 0) {
                     $data[$country][$i]['length'] = 0;
                 }
+                $data[$country][$i]['top'] = $top;
             }
         }
 //        return $data;
--- app/views/ppm.blade.php
+++ app/views/ppm.blade.php
@@ -14,7 +14,7 @@
     <tr>
         <td><img src="/images/32/{{$c}}.png">&nbsp;{{$c}}<br/><a href="http://www.betexplorer.com/soccer/{{$d[1]['country']}}/{{$d[1]['name']}}">[be]</a>&nbsp;<a href="http://www.sportstats.com/soccer/{{$d[1]['country']}}/{{$d[1]['name']}}">[ss]</a>&nbsp;<a>[sc]</a></td>
             @for($i = 1; $i < 11; $i ++)
-                <td class="text-center">{{$d[$i]['length']}}</td>
+                <td class="text-center hasTooltip" title="{{$d[$i]['top']}}">{{$d[$i]['length']}}</td>
             @endfor
     </tr>
     @endforeach
