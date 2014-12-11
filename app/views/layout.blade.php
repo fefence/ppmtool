8fb47a84d51c9668995a6311b803cb910719b8a5
veseto <wpopowa@gmail.com>
2014-12-11 19:57:53 +0200

    log

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -58,7 +58,7 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Tools<span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Action log</a></li>
+                            <li><a href="/log">Action log</a></li>
 
                             <li><a href="/stats">Statistics</a></li>
                             <li><a href="/live">Livescore</a></li>
