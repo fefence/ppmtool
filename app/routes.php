a6e5ffc68788deae5b4f2fa4542d4ef906cfd20a
veseto <wpopowa@gmail.com>
2014-12-27 16:02:53 +0200

    boo

--- app/routes.php
+++ app/routes.php
@@ -13,15 +13,15 @@
 
 Route::get('/boo/{id}/{season}', function($id, $season)
 {
-    return Parser::parseMatchesForLeagueAndSeason($id, $season);
-//    $matches_parsed = WinOdds::lists('match_id');
-//    $matches = Match::whereNotIn('id',$matches_parsed)
-//        ->where('season', $season)
-//        ->where('league_id', $id)
-//        ->get();
-//    foreach($matches as $m) {
-//        echo OddsParser::parseOdds($m);
-//    }
+//    return Parser::parseMatchesForLeagueAndSeason($id, $season);
+    $matches_parsed = WinOdds::lists('match_id');
+    $matches = Match::whereNotIn('id',$matches_parsed)
+        ->where('season', $season)
+        ->where('league_id', $id)
+        ->get();
+    foreach($matches as $m) {
+        echo OddsParser::parseOdds($m);
+    }
 });
 Route::get('/list/{fromdate?}/{todate?}', 'LivescoreController@livescore');
 Route::get('/test', 'LivescoreController@test');
