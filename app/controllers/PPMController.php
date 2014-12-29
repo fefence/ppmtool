c3f25da26d7e57c42048ad78a5b153b6aacd084d
veseto <wpopowa@gmail.com>
2014-12-29 21:08:54 +0200

    series

--- app/controllers/PPMController.php
+++ app/controllers/PPMController.php
@@ -1,16 +1,18 @@
 <?php
 
 
-class PPMController extends \BaseController{
+class PPMController extends \BaseController
+{
 
-    public static function displaySeries() {
+    public static function displaySeries()
+    {
         $countries = League::where('hidden', 0)
             ->orderBy('country')
             ->lists('country_alias');
         $games = GameType::all();
         $data = array();
-        foreach($countries as $country) {
-            for($i = 1; $i < 11; $i ++) {
+        foreach ($countries as $country) {
+            for ($i = 1; $i < 11; $i++) {
                 $top = '';
                 $top_25 = Series::where('country_alias', $country)
                     ->join('leagues', 'leagues.id', '=', 'series.league_id')
@@ -31,8 +33,8 @@ class PPMController extends \BaseController{
 //                    ->take(25)
                     ->get();
                 $c = '';
-                foreach($current as $t) {
-                    $c = $c.$t->length.", ";
+                foreach ($current as $t) {
+                    $c = $c . $t->length . ", ";
                 }
 
                 $data[$country][$i] = Series::where('active', 1)
@@ -46,24 +48,26 @@ class PPMController extends \BaseController{
                     $data[$country][$i] = new Series;
                 }
                 $k = 0;
-                foreach($top_25 as $t) {
-                    $k ++;
+                foreach ($top_25 as $t) {
+                    $k++;
                     if ($current->contains($t->id)) {
                         if ($t->id == $data[$country][$i]->id) {
-                            $top = $top."<span class='bg-danger text-danger' style='font-weight: bold;'>".$t->length."</span>, ";
+                            $top = $top . "<span class='bg-danger text-danger' style='font-weight: bold;'>" . $t->length . "</span>, ";
                         } else {
-                            $top = $top."<span class='bg-info text-info' style='font-weight: bold;'>".$t->length."</span>, ";
+                            $top = $top . "<span class='bg-info text-info' style='font-weight: bold;'>" . $t->length . "</span>, ";
                         }
                     } else {
-                        $top = $top.$t->length.", ";
+                        $top = $top . $t->length . ", ";
                     }
-                    if ($k >= 15) {
+                    if ($k > 15) {
                         continue;
+                    } else if ($k == 15) {
+                        $top = $top . "<span style='color: #aaa;'>";
                     } else {
                         $data[$country][$i]['treshold'] = $t->length;
                     }
                 }
-                $data[$country][$i]['top'] =  substr($top, 0, strlen($top) - 2);
+                $data[$country][$i]['top'] = substr($top, 0, strlen($top) - 2) . "</span>";
                 $data[$country][$i]['curr'] = substr($c, 0, strlen($c) - 2);
 
             }
@@ -72,7 +76,9 @@ class PPMController extends \BaseController{
         return View::make('ppm')->with(['data' => $data, 'games' => $games]);
     }
 
-    public static function displaySeriesGames($series_id) {
+    public
+    static function displaySeriesGames($series_id)
+    {
         $user_id = Auth::user()->id;
         $games = Game::where('user_id', $user_id)
             ->join('matches', 'matches.id', '=', 'games.match_id')
@@ -82,7 +88,7 @@ class PPMController extends \BaseController{
             ->select(DB::raw('games.*, matches.home, matches.away, matches.date_time, matches.home_goals, matches.away_goals, matches.short_result'))
             ->orderBy('current_length', "desc")
             ->get();
-        if(count($games) > 0) {
+        if (count($games) > 0) {
             $no_info = false;
         } else {
             $no_info = true;
