e7788215e6ff7827b2b071718ac0a0916aca1ce6
veseto <wpopowa@gmail.com>
2015-01-11 12:38:57 +0200

    fix livescore -2 hours from previous day

--- app/helpers/Utils.php
+++ app/helpers/Utils.php
@@ -3,16 +3,16 @@
 class Utils {
     public static function calcDates($fromdate, $todate) {
         if ($todate == '' && $fromdate == '') {
-            $fromdate = \Carbon\Carbon::now()->startOfDay();
+            $fromdate = \Carbon\Carbon::now()->startOfDay()->subHours(2);
             $todate = \Carbon\Carbon::now()->endOfDay()->addHours(11)->addMinutes(30);
         } else if($todate == '') {
             $arr = explode('-', $fromdate);
-            $fromdate = \Carbon\Carbon::createFromDate($arr[0], $arr[1], $arr[2])->startOfDay();
+            $fromdate = \Carbon\Carbon::createFromDate($arr[0], $arr[1], $arr[2])->startOfDay()->subHours(2);
             $todate = $fromdate->endOfDay()->addHours(9);
         } else {
             $arr = explode('-', $fromdate);
             $arr1 = explode('-', $todate);
-            $fromdate = \Carbon\Carbon::createFromDate($arr[0], $arr[1], $arr[2])->startOfDay();
+            $fromdate = \Carbon\Carbon::createFromDate($arr[0], $arr[1], $arr[2])->startOfDay()->subHours(2);
             $todate = \Carbon\Carbon::createFromDate($arr1[0], $arr1[1], $arr1[2])->endOfDay()->addHours(11)->addMinutes(30);
         }
         return [$fromdate, $todate];
