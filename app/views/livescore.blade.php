2505a3782e9164c97264f2ac5c1704410bb57fa9
fefence <dummy@asd.asd>
2015-01-24 19:37:53 +0200

    asd

--- app/views/livescore.blade.php
+++ app/views/livescore.blade.php
@@ -12,12 +12,13 @@
             } else {
                 $url = '/listbycountry/'.$from.'/'.$from;
             }
+            $m = null;
         ?>
         <p><a href="{{$url}}" role="button" class="btn btn-default">country</a></p>
         <table class="table">
             <tbody>
             @foreach($matches as $d)
-            <tr id="{{$d['match']->id}}">
+            <tr id="{{$d['match']->id}}" @if($m != null && date('Y-m-d', strtotime($m->date_time)) != date('Y-m-d', strtotime($d['match']->date_time))) class="border-bottom-overnight" @endif>
                 <td style="width: 50px;"><img src="/images/32/{{$d['league']->country_alias}}.png"></td>
                 <td style="width: 50px;">{{date('H:i', strtotime($d['match']->date_time) - 3600)}}</td>
                 <td style="text-align: right;" class="home redcard{{$d['match']->home_red}} right">{{$d['match']->home}}</td>
@@ -30,6 +31,9 @@
                 </td>
                 <td>@if(count($d['settings'])>0 && $d['match']->short_result != '-' && $d['match']->home_goals == 0 && $d['match']->away_goals == 0)<a href="/refund/{{$d['match']->id}}" role="button" class="btn btn-xs btn-warning" @if($d['refund'] <= 0) disabled @endif>refund</a>@endif</td>
             </tr>
+            <?php
+             $m = $d['match'];
+            ?>
             @endforeach
             </tbody>
         </table>
