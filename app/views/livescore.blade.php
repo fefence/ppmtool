dc7c983cbdf15c41959183926c0f31adff5897f2
veseto <wpopowa@gmail.com>
2015-03-08 17:35:16 +0200

    fix2

--- app/views/livescore.blade.php
+++ app/views/livescore.blade.php
@@ -30,7 +30,7 @@
                     @endforeach
 
                     @foreach($d['placeholders'] as $pl)
-                    <a href="/play/{{date('Y-m-d', strtotime($d['match']->date_time))}}/{{date('Y-m-d', strtotime($d['match']->date_time))}}/#{{$d['league']->country_alias}}" role="button" class="btn btn-default btn-xs hasTooltip" title="{{$pl->s}} @ {{$pl->odds}}"  @if($pl->s == 0) disabled @endif>{{$s->game_type->name}}</a>
+                    <a href="/play/{{date('Y-m-d', strtotime($d['match']->date_time))}}/{{date('Y-m-d', strtotime($d['match']->date_time))}}/#{{$d['league']->country_alias}}" role="button" class="btn btn-default btn-xs hasTooltip" title="{{$pl->s}} @ {{$pl->odds}}"  @if($pl->s == 0) disabled @endif>{{$pl->game_type->name}}</a>
                     @endforeach
                 </td>
                 <td>@if(count($d['settings'])>0 && $d['match']->short_result != '-' && $d['match']->home_goals == 0 && $d['match']->away_goals == 0)<a href="/refund/{{$d['match']->id}}" role="button" class="btn btn-xs btn-warning" @if($d['refund'] <= 0) disabled @endif>refund</a>@endif</td>
